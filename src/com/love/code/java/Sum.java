54bcff28c5cfab1f0e83c5e8886836fd73dbc3a1
Pradeep Ilango (PI) <pradei@amazon.com>
2020-06-27 21:55:19 +0530

    Added Sum class which adds two integers

---
+++ src/com/love/code/java/Sum.java
@@ -0,0 +1,11 @@
+package com.love.code.java;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println("The sum of the numbers is: " + a + b);
+    }
+
+}
