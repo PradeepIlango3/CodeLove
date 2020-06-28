cf8483e41213493e42472c6b7f620a3bcf3cec3b
Pradeep Ilango (PI) <pradei@amazon.com>
2020-06-28 11:00:22 +0530

    Added Cop and Thief class

---
+++ src/com/love/code/java/crime/Thief.java
@@ -0,0 +1,12 @@
+package com.love.code.java.crime;
+
+import com.love.code.java.justice.Cop;
+
+public class Thief {
+
+    public static void main(String[] args) {
+        Cop cop = new Cop();
+        cop.fire();
+    }
+
+}
---
+++ src/com/love/code/java/justice/Cop.java
@@ -0,0 +1,14 @@
+package com.love.code.java.justice;
+
+public class Cop {
+
+    private int gun;
+
+    public int getGun() {
+        return gun;
+    }
+    public void fire() {
+        System.out.println("shoot!");
+    }
+
+}
